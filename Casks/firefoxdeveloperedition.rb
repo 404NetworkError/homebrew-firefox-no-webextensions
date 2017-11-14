6f09d11701f40dfe7cf7b52f0c10111583616216
404NetworkError <fake@email.address>
2017-11-13 19:08:17 -0600

    Update firefoxdeveloperedition 56.0b12
    Matches caskroom/homebrew-versions@10a8f30

--- Casks/firefoxdeveloperedition.rb
+++ Casks/firefoxdeveloperedition.rb
@@ -1,50 +1,50 @@
 cask 'firefoxdeveloperedition' do
-  version '56.0b8'
+  version '56.0b12'
 
   language 'cs' do
-    sha256 '3e3f01dc1e6a6ef00c720bc95ea8719fe81f7574e87bc16947e5c89ea2261547'
+    sha256 '4d403e367bff67bfe57a9e2c6ac3a4226002bac7999b98a9d55b81fcd72c87fb'
     'cs'
   end
 
   language 'de' do
-    sha256 'c4c8c8c5ca2fd10fcf941637c07f1623c6cdee78f23d9f7db59d0f7203e1443d'
+    sha256 'd25dd77f9a8fd95ed6d820b149d3f482fd3adbf01b4302ff5f6d25c3ca6c1a4c'
     'de'
   end
 
   language 'en', default: true do
-    sha256 'fda509f5164dd555472633fa502193de75cbf841465d9d037d1d814f44a9c9ab'
+    sha256 'a41d36bc9d827c541246ff59d34655204e1d779fdcb2429751e7f207bc3e56d8'
     'en-US'
   end
 
   language 'ja' do
-    sha256 'a31f01bb2018c03d0dae222255f1a6dc8b76b10cc41e87e3276e78282877ee57'
+    sha256 'a685c20e5145b37a70081ab51944a7645220d01e46f2e823f32054d46f2f78ca'
     'ja-JP-mac'
   end
 
   language 'ru' do
-    sha256 '16afe13bf1e51181b1873c5c125d22597b3865df283d58cb43934be7fde184c8'
+    sha256 '55630999147610037ee9ecc856be9cfdcb343e1a1a0694c16bf31e6f0fe6b5a9'
     'ru'
   end
 
   language 'uk' do
-    sha256 '4d45ddaded6257695e40bc0351c60b92f2bbac901799ab8c22d3d8d911f2dc72'
+    sha256 'e36b49e60fa8408357e53cc43893791c94347df5ad27dfe85389bb46c0cff104'
     'uk'
   end
 
   language 'zh-TW' do
-    sha256 '7e72629031a5353f29e4e0f4dbbd5a56635619f1e28bb53b64eeb644de7e078f'
+    sha256 '1d42018aae5494e2265eaac709dcf24610f78baf71267210ec74286865b7b67b'
     'zh-TW'
   end
 
   language 'zh' do
-    sha256 '5314aa05746acc6df1d28a3953392a605a5905079ccca66a0402446cc423cfb0'
+    sha256 '82434ae33f46db95debe7a7c5d03e07324263c6b19b1c6e344afc3d29a0c6e5e'
     'zh-CN'
   end
 
   # download-installer.cdn.mozilla.net/pub/devedition/releases was verified as official when first introduced to the cask
   url "https://download-installer.cdn.mozilla.net/pub/devedition/releases/#{version}/mac/#{language}/Firefox%20#{version}.dmg"
   appcast 'https://download-installer.cdn.mozilla.net/pub/devedition/releases/',
-          checkpoint: 'e805f4fa5a8f5139ea6c053bfb41e35003981356e2e32f229d5edf20a999b30a'
+          checkpoint: '03ed2da848c0ae523ba3749f41a9bef10d8ce5da5cc8128e763ca11b5cee85f7'
   name 'Mozilla Firefox Developer Edition'
   homepage 'https://www.mozilla.org/firefox/developer/'
 
